50049970d6a40806e93380538b14b0583682aeab
reblou <rebeayto@gmail.com>
2018-03-27 22:42:54 +0100

    Technique by weight category function

--- statistics.py
+++ statistics.py
@@ -11,6 +11,7 @@ def avg_height():
     for r in res:
         print(r[0], ":", round(r[1] / r[2]), "cm")
     conn.close()
+    print()
     return
 
 def most_popular_techniques():
@@ -21,7 +22,31 @@ def most_popular_techniques():
     print("Technique : Frequency")
     for r in res:
         print(r[1], ":", r[0])
+    conn.close()
+    print()
+    return
+
+def technique_by_category():
+    conn = psycopg2.connect(database="judoka")
+    cur = conn.cursor()
+
+    try:
+        cur.execute("DROP VIEW frequencies")
+    except:
+        pass
+
+    cur.execute("CREATE VIEW frequencies AS (SELECT COUNT(*), categories, ftechnique FROM judoka GROUP BY ftechnique, categories)")
+    cur.execute("SELECT max, a.categories, a.ftechnique FROM frequencies a INNER JOIN (SELECT categories, MAX(count) FROM frequencies as t GROUP BY categories) as b ON a.categories = b.categories AND a.count = b.max")
+    res = cur.fetchall()
+    for r in res:
+        print(r)
+    conn.close()
+    print()
+    return
+
+
 
 if __name__ == "__main__":
     avg_height()
     most_popular_techniques()
+    technique_by_category()
