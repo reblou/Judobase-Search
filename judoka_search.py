fffb257c5b4f83e2420f3565ac6be20cf80cc370
reblou <rebeayto@gmail.com>
2018-04-03 08:17:34 +0100

    Basic searching

--- judoka_search.py
+++ judoka_search.py
@@ -3,15 +3,25 @@
 import psycopg2
 import sys
 
-def search_db(args):
-    query = """ SELECT * FROM judoka WHERE (ftechnique = %s AND height=%s AND gender=%s AND categories=%s)"""
-    cur.execute(query, (args[0], args[1], args[2], args[3]))
+def search_technique(technique):
+    conn = psycopg2.connect(database="judoka")
+    cur = conn.cursor()
+    query = """ SELECT * FROM judoka WHERE (ftechnique = %s)"""
+    cur.execute(query, (technique,))
+    return cur.fetchall()
 
 if __name__ == "__main__":
     try:
-        args = sys.argv
-        print(args)
-    except:
-        print("Please enter arguments")
+        ars = sys.argv
+        print(ars)
+        if (len(ars) < 5):
+            raise Exception('Not enough arguments')
+        ars.pop(0)
+        res = search_technique(ars[0])
+        print(res)
+
+    except Exception as e:
+        print(str(e))
+        print("Please enter arguments:")
         print("[Fav. technique] [Height] [Gender] [Category]")
         exit()
